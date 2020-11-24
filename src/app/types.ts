0231aa1c9284cb9df654f54df275577de2f9fa75
kiingdavies <daviesadedeji007@gmail.com>
2020-11-24 13:19:06 +0100

    Poll Create components completed

---
+++ src/app/types.ts
@@ -0,0 +1,12 @@
+export interface Poll {
+    id: number; // 12
+    question: string[]; // which days of week you like most
+    results: number[]; // [0,0,0,0,5,7,2]
+    options: string[]; // ['monday','Tuesday', 'wednesday'....]
+    thumbnail: string; // https;//image.png
+}
+
+export interface Voter {
+    id: string; // 0xJhdgfkeh
+    voted: number[]; // [12]
+}
