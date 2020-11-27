b5ef11b987e940d82f62b4db5b1d7ccf334224c0
kiingdavies <daviesadedeji007@gmail.com>
2020-11-27 15:26:00 +0100

    PollVote component results done

--- src/app/poll-vote/poll-vote.component.ts
+++ src/app/poll-vote/poll-vote.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-poll-vote',
@@ -7,7 +8,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./poll-vote.component.scss'],
 })
 export class PollVoteComponent implements OnInit {
-  options = ['Monday', 'Tuesday', 'wednesday'];
+  @Input() voted: boolean;
+  @Input() options: string[];
+  @Input() results: number[];
+  @Input() question: string;
+
   voteForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
@@ -18,9 +23,50 @@ export class PollVoteComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
-
+  ngOnInit(): void {
+    // generate the chart if the user has voted
+    if (this.voted) {
+      this.generateChart();
+    }
+  }
+  // submit poll-vote form function
   submitForm() {
     console.log(this.voteForm.value);
+  }
+
+  // generate apex chart function in poll-vote.component.html this defines the options for our chart
+  generateChart() {
+    const options: ApexCharts.ApexOptions = {
+      series: [
+        {
+          data: this.results, // [5, 7, 10]
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          distributed: true,
+        },
+      },
+      legend: {
+        show: false,
+      },
+      xaxis: {
+        categories: this.options, // options = [{'Monday', 'Tuesday', 'wednesday'}];
+      },
+    };
+
+    // This gets the tag to render the element in
+    const chart = new ApexCharts(
+      document.getElementById('poll-results'),
+      options
+    );
+
+    // This renders the chart
+    chart.render();
   }
 }
