fba724ad1c20b479d456d785d9cdaeb1388d9aa6
kiingdavies <daviesadedeji007@gmail.com>
2020-11-27 10:45:06 +0100

    poll component render done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   // this enables the Create Poll button to shows the pollForm when clicked in the app-poll-create html tag in app.component.html file
   showForm = false;
+
+  polls = [
+    {
+      question: 'Do you like bitcoins?',
+      image:
+        'https://render.fineartamerica.com/images/rendered/search/acrylic-print/10/6.5/hangingwire/break/images/artworkimages/medium/1/5-cryptocurrency-hologram-and-circuit-board-allan-swart.jpg',
+      votes: [0, 5, 7, 1],
+      voted: false,
+    },
+    {
+      question: 'Is roanldo the best player in the world?',
+      image:
+        'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec593cc431fb70007482137%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1321%26cropX2%3D3300%26cropY1%3D114%26cropY2%3D2093',
+      votes: [5, 4, 2],
+      voted: true,
+    },
+  ];
 }
--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
-  styleUrls: ['./poll.component.scss']
+  styleUrls: ['./poll.component.scss'],
 })
 export class PollComponent implements OnInit {
+  @Input() question: string;
+  @Input() votes: number[]; // [0,1,5,7]
+  @Input() voted: boolean;
+  @Input() pollImage: string;
 
-  constructor() { }
+  numberOfVotes: number;
+  // Loggedin: boolean;
+
+  constructor() {}
 
   ngOnInit(): void {
+     // This checks if theres any vote in the vote array, it sets acc to 0 initially if theres curr it adds and changes state of acc
+     if (this.votes.length) {
+      this.numberOfVotes = this.votes.reduce((acc, curr) => {
+        return (acc += curr);
+      });
+    }
   }
-
 }
